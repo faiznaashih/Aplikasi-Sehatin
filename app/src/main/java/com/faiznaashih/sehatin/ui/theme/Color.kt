8adb3a56fdbd90cfc8b5990d1a698f7b2034ee18
M. Faiz Naashih Rozaq <137278344+faiznaashih@users.noreply.github.com>
2025-08-06 14:44:15 +0700

    first commit

---
+++ app/src/main/java/com/faiznaashih/sehatin/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.waseefakhtar.doseapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFF3203C4)
+val PurpleGrey80 = Color(0xFF5C3993)
+val Pink80 = Color(0xFF8C2442)
+
+val Purple40 = Color(0xFF1004B0)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
